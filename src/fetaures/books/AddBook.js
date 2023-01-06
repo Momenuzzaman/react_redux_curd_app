fd62abb9dd2e688c7d1999adc6901b985d1f89f8
mdemon420 <mdeamon4@gmail.com>
2023-01-06 19:43:26 +0600

    create form

--- src/fetaures/books/AddBook.js
+++ src/fetaures/books/AddBook.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddBook = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
     return (
         <div>
-            <h1>Add book</h1>
+            <h2>Add book</h2>
+            <form >
+                <div className="form-field">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text"
+                        id="title"
+                        name="title"
+                        value={title}
+                        required placeholder="Enter Book Title"
+                        onChange={(e) => setTitle(e.target.value)}>
+                    </input>
+                </div>
+                <div className="form--field">
+                    <label htmlFor="author">Author:</label>
+                    <input type="text"
+                        id="author"
+                        name="author"
+                        value={author}
+                        required placeholder="Enter Book Title"
+                        onChange={(e) => setAuthor(e.target.value)}>
+                    </input>
+                </div>
+                <button type="submit">Add</button>
+            </form>
         </div>
     );
 };
