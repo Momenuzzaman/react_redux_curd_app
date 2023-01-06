0559fbdfdc3fd77c92e5062bf8a78695139e1b4b
mdemon420 <mdeamon4@gmail.com>
2023-01-06 18:54:54 +0600

    create table

--- src/fetaures/books/BooksView.js
+++ src/fetaures/books/BooksView.js
@@ -6,7 +6,32 @@ const BooksView = () => {
     console.log(books);
     return (
         <div>
-            <h2>List of books</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books && books.map(book => {
+                        const { id, title, author } = book;
+                        return (
+                            <tr>
+                                <td>{id}</td>
+                                <td>{title}</td>
+                                <td>{author}</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 };
